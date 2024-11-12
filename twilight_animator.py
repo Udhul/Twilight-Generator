b9063b1f37bf3baf240077708d82bd6ce231a3b1
Udhul <oldengloe@gmail.com>
2024-11-12 04:03:40 +0100

    Make Animator use Timeline

--- twilight_animator.py
+++ twilight_animator.py
@@ -134,41 +134,27 @@ class TwilightAnimator(QObject):
     frame_generated = Signal(int, TwilightState)  # Signal emitting (frame_number, interpolated_state)
     animation_finished = Signal()  # Signal emitted when animation completes
 
-    def __init__(self, keyframes, direction='forward', framerate=30):
+    def __init__(self, timeline: Timeline = None, direction='forward'):
         """
-        Initializes the TwilightAnimator with keyframes, interpolation direction, and framerate.
+        Initializes the TwilightAnimator with a Timeline instance.
 
         Parameters:
-        - keyframes (list of tuples): Each tuple should be (frame_number (int), TwilightState).
+        - timeline (Timeline): Timeline containing keyframes and framerate settings
         - direction (str): 'forward' or 'backward'. Determines interpolation direction for cyclical attributes.
-        - framerate (int): Frames per second. Determines the delay between frame generations.
         """
         super().__init__()
 
-        if not keyframes:
-            raise ValueError("Keyframes list cannot be empty.")
-
-        # Sort keyframes by frame number
-        self.keyframes = sorted(keyframes, key=lambda kf: kf[0])
-
-        # Check for duplicate frame numbers
-        frames = [kf[0] for kf in self.keyframes]
-        if len(frames) != len(set(frames)):
-            raise ValueError("Duplicate frame numbers detected in keyframes.")
-
+        self.timeline = timeline if timeline else Timeline([])
+        
         # Validate direction
         self.direction = direction.lower()
         if self.direction not in ['forward', 'backward']:
             raise ValueError("Interpolation direction must be 'forward' or 'backward'.")
 
-        # Validate framerate
-        if not isinstance(framerate, (int, float)) or framerate <= 0:
-            raise ValueError("Framerate must be a positive number.")
-        self.framerate = framerate
-        self.frame_delay = 1.0 / self.framerate  # Seconds per frame
+        self.frame_delay = 1.0 / self.timeline.framerate
+        self._is_running = False
+        self._current_frame = self.timeline.start_frame
 
-        self._is_running = False  # Control flag for animation loop
-        self._current_frame = self.keyframes[0][0]  # Initialize current frame
 
     @Slot()
     def run_animation(self):
@@ -193,7 +179,6 @@ class TwilightAnimator(QObject):
         """
         self._is_running = False
 
-# TODO: FIX for actual interpolated frame
     def set_current_frame(self, frame_number):
         """
         Sets the current frame to start the animation from.
@@ -201,57 +186,64 @@ class TwilightAnimator(QObject):
         Parameters:
         - frame_number (int): The frame number to start from.
         """
-        # Find the closest keyframe less than or equal to the desired frame
-        for i, (kf_frame, _) in enumerate(self.keyframes):
-            if kf_frame > frame_number:
-                if i == 0:
-                    self._current_frame = self.keyframes[0][0]
-                else:
-                    self._current_frame = self.keyframes[i - 1][0]
-                break
-        else:
-            # If frame_number is beyond the last keyframe
-            self._current_frame = self.keyframes[-1][0]
+
+        target_state = self.timeline.get_state_at_frame(frame_number)
+        if target_state:
+            self._current_frame = frame_number
+            return target_state
 
     def sequence_generator(self):
         """
-        Generator that yields interpolated TwilightState instances between keyframes.
+        Generator that yields interpolated TwilightState instances between keyframes on the timeline.
 
         Yields:
         - tuple: (frame_number (int), TwilightState instance)
         """
-        num_keyframes = len(self.keyframes)
-        for i in range(num_keyframes - 1):
-            start_frame, start_state = self.keyframes[i]
-            end_frame, end_state = self.keyframes[i + 1]
+        current = self._current_frame
+        while current <= self.timeline.end_frame and self._is_running:
+            state = self.timeline.get_state_at_frame(current)
+            if state:
+                yield (current, state)
+            current += 1
 
-            if end_frame <= start_frame:
-                continue  # Skip invalid keyframes
+    # def sequence_generator(self, start_at_frame=0):
+    #     """
+    #     Generator that yields interpolated TwilightState instances between keyframes.
 
-            # Number of frames to interpolate
-            total_steps = end_frame - start_frame
+    #     Yields:
+    #     - tuple: (frame_number (int), TwilightState instance)
+    #     """
+    #     num_keyframes = len(self.keyframes)
+    #     for i in range(num_keyframes - 1):
+    #         start_frame, start_state = self.keyframes[i]
+    #         end_frame, end_state = self.keyframes[i + 1]
 
-            for step in range(total_steps):
-                t = step / total_steps if total_steps > 0 else 1
-                interpolated_state = interpolate_states(start_state, end_state, t)
-                current_frame = start_frame + step
-                yield (current_frame, interpolated_state)
+    #         if end_frame <= start_frame:
+    #             continue  # Skip invalid keyframes
 
-        # Emit the last keyframe
-        last_frame, last_state = self.keyframes[-1]
-        yield (last_frame, last_state.copy())
+    #         # Number of frames to interpolate
+    #         total_steps = end_frame - start_frame
+
+    #         # TODO: get current abs frame and skip to start frame before generating states
+
+    #         for step in range(total_steps):
+    #             t = step / total_steps if total_steps > 0 else 1
+    #             interpolated_state = interpolate_states(start_state, end_state, t)
+    #             current_frame = start_frame + step
+    #             yield (current_frame, interpolated_state)
+
+    #     # Emit the last keyframe
+    #     last_frame, last_state = self.keyframes[-1]
+    #     yield (last_frame, last_state.copy())
 
 class AnimationThread(QThread):
     """
     QThread subclass to run TwilightAnimator in a separate thread.
     """
 
-    def __init__(self, keyframes, direction='forward', framerate=30, parent=None):
+    def __init__(self, timeline: Timeline, direction='forward', parent=None):
         super().__init__(parent)
-        self.keyframes = keyframes
-        self.direction = direction
-        self.framerate = framerate
-        self.animator = TwilightAnimator(self.keyframes, self.direction, self.framerate)
+        self.animator = TwilightAnimator(timeline, direction)
         self.animator.moveToThread(self)
 
     def run(self):
